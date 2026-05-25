39b687bd28ebf32cc567849502de797168fc7324
Jonatan Pintos <136272535+muyunicos@users.noreply.github.com>
2026-05-25 19:55:24 -0300

    ok

--- inc/geo.php
+++ inc/geo.php
@@ -383,7 +383,12 @@ if ( ! function_exists( 'mu_country_modal_html' ) ) {
         <div id="muyu-country-modal-overlay" class="mu-modal-overlay" data-current-domain="<?php echo esc_attr( $current_domain ); ?>">
             <div id="muyu-country-modal" class="mu-modal-container">
                 <div class="mu-modal-content">  
-                    <button id="muyu-country-close" class="mu-modal-close" title="Cerrar" aria-label="Cerrar">&times;</button>
+                   <button id="muyu-country-close" class="mu-modal-close" type="button" aria-label="Cerrar">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+</button>
                     <div>
                         <div>
                             <?php echo esc_html( $modal_question ); ?>
